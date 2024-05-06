a69501e5ff270193fbfe4a89594e34f172d57beb
JophielV <jophielvillanueva@gmail.com>
2024-05-06 19:14:00 +0800

    Swagger

--- notes-app/src/main/java/com/notes/app/SpringFoxConfig.java
+++ notes-app/src/main/java/com/notes/app/SpringFoxConfig.java
@@ -1,16 +1,18 @@
 package com.notes.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig {
-
-    public Docket SwaggerApi() {
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -18,3 +20,4 @@ public class SwaggerConfig {
                 .build();
     }
 }
+
